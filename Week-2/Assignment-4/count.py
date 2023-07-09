77e1649278aa91eb7ec0384e67fca4c2a53a3a19
xyjameschen <138367253+xyjameschen@users.noreply.github.com>
2023-07-09 20:51:13 +0800

    Add files via upload

--- Week-2/Assignment-4/count.py
+++ Week-2/Assignment-4/count.py
@@ -1,1 +1,15 @@
+def count(input_list):
+    
+    unique_list = []
+    results = {}
 
+    for letter in input_list:
+        if letter not in unique_list:
+            unique_list.append(letter)
+        else:
+            continue
+    
+    for element in unique_list:
+        results[element] = input_list.count(element)
+    
+    return results
