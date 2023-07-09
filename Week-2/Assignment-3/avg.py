2bff21330c5501506f3ae9de521f730ed474cbf3
xyjameschen <138367253+xyjameschen@users.noreply.github.com>
2023-07-09 20:50:23 +0800

    Add files via upload

--- Week-2/Assignment-3/avg.py
+++ Week-2/Assignment-3/avg.py
@@ -1,1 +1,10 @@
+def avg(products_dic):
 
+    total = len(products_dic['products'])
+    price_list = []
+
+    for dic in products_dic['products']:
+        price_list.append(dic['price'])
+    
+    results = round((sum(price_list) / total), 3)
+    return results
