fe5bf1de40fdc27ce858422906e1bbbf0504d378
xyjameschen <138367253+xyjameschen@users.noreply.github.com>
2023-07-10 11:52:50 +0800

    update

--- Week-2/Assignment-4/count.py
+++ Week-2/Assignment-4/count.py
@@ -1,15 +1,8 @@
 def count(input_list):
     
-    unique_list = []
     results = {}
 
-    for letter in input_list:
-        if letter not in unique_list:
-            unique_list.append(letter)
-        else:
-            continue
-    
-    for element in unique_list:
+    for element in set(input_list):
         results[element] = input_list.count(element)
     
     return results
--- Week-2/Assignment-4/group_by_key.py
+++ Week-2/Assignment-4/group_by_key.py
@@ -6,16 +6,12 @@ def group_by_key(input_list):
     for dic in input_list:
         if dic['key'] not in unique_keys:
             unique_keys.append(dic['key'])
-        else:
-            continue
     
     for letter in unique_keys:
         total = 0
         for dic in input_list:
             if letter == dic['key']:
                 total += dic['value']
-            else:
-                continue
         results_dic[letter] = total
     
     return results_dic
